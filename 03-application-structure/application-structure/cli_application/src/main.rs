c97f70e944eddc562aed3c926988cf3bef1b5d5c
Sandor Apati <s.apati@mito.hu>
2024-02-01 07:31:11 +0100

    application structure, cli

---
+++ 03-application-structure/application-structure/cli_application/src/main.rs
@@ -0,0 +1,12 @@
+use clap::Command;
+use cli_application::commands;
+
+fn main() -> anyhow::Result<()> {
+    let mut command = Command::new("Sample CLI application");
+    command = commands::configure(command);
+
+    let matches = command.get_matches();
+    commands::handle(&matches)?;
+
+    Ok(())
+}
